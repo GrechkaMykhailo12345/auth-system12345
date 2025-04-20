591c00a75f278f35d9cfb5f7510f7bbae75f79f4
GrechkaMykhailo12345 <yalta010709@gmail.com>
2025-04-20 14:10:16 +0300

    second commit

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
-"""
- _______       _            _     _          ______        _                 _ 
-(_______)     (_)       _  (_)   | |        (____  \      (_)               | |
- _______  ____ _  ___ _| |_ _  __| |_____    ____)  ) ____ _ _____ ____   __| |
-|  ___  |/ ___) |/___|_   _) |/ _  | ___ |  |  __  ( / ___) (____ |  _ \ / _  |
-| |   | | |   | |___ | | |_| ( (_| | ____|  | |__)  ) |   | / ___ | | | ( (_| |
-|_|   |_|_|   |_(___/   \__)_|\____|_____)  |______/|_|   |_\_____|_| |_|\____|
-    
-Auteur: Miha(miha.pro@gmail.com) 
-main.py(Ɔ) 2025
-Description : Saisissez la description puis « Tab »
-Créé le :  dimanche 20 avril 2025 à 14:06:21 
-Dernière modification : 
-"""
+from fastapi import FastAPI
+from pydantic import BaseModel
 
+app = FastAPI()
+
+users = [
+    {
+        'login': "admin",
+        'name': 'Volodymyr',
+        'surname': 'Hubash',
+        'age': 25,
+    }
+]
+
+
+class User(BaseModel):
+    login: str
+    name: str
+    surname: str
+    age: int
+    
+
+@app.get("/users/all")
+async def get_all_users():
+    """
+    Повертає дані про всіх користувачів
+    """
+    return users
+
+
+@app.post("/users/new/")
+async def add_new_user(new_user: User):
+    """
+    Додає нового користувача з login, name, surname, age
+    """
+    users.append({
+        "login": new_user.login,
+        "name": new_user.name,
+        "surname": new_user.surname,
+        "age": new_user.age,
+    })
+
+    return {'message': "Користувача додано"}
